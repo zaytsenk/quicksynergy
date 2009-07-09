a0e1a49c933b122c781ae159986f96303eb51105
otavio <otavio@279d60f0-6c25-11de-afdd-bb9f892af8c1>
2009-07-09 03:23:45 +0000

    - Branches initialization

---
+++ tags/osx/QS_1_0/QuickSynergyController.h
@@ -0,0 +1,34 @@
+//
+//  QuickSynergyController.h
+//  QuickSynergy
+//
+//  Created by Otavio Cordeiro on 9/6/08.
+//  Copyright 2008 Otávio Cordeiro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <Growl/Growl.h>
+#import "SynergyHelper.h"
+
+#define GROWL_QS_STARTED @"Started"
+#define GROWL_QS_STOPPED @"Stopped"
+#define GROWL_QS_ERROR   @"Error"
+
+#define TAB_SHARE @"Share"
+#define TAB_USE   @"Use"
+
+@interface QuickSynergyController : NSObject <GrowlApplicationBridgeDelegate>
+{
+    SynergyHelper *synergy;
+    
+    IBOutlet NSTextField *clientAbove;
+    IBOutlet NSTextField *clientBelow;
+    IBOutlet NSTextField *clientLeft;
+    IBOutlet NSTextField *clientRight;
+    IBOutlet NSTextField *serverAddr;
+    IBOutlet NSButton *startStopButton;
+    IBOutlet NSTabView *tabview;
+}
+
+- (IBAction)startStop:(id)sender;
+@end
