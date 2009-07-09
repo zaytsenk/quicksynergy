66a7e9cbdf2d7ebce326894f89c19e6d828e2a31
otavio <otavio@279d60f0-6c25-11de-afdd-bb9f892af8c1>
2009-07-09 22:41:50 +0000

    - Added copyright and GPL version 2 Terms and Conditions.

--- osx/SynergyHelper.m
+++ osx/SynergyHelper.m
@@ -1,11 +1,22 @@
-//
-//  SynergyHelper.m
-//  QuickSynergy
-//
-//  Created by Otavio Cordeiro on 9/6/08.
-
-//  Copyright 2009 Otávio Cordeiro. All rights reserved.
-//
+/*
+ * QuickSynergy: Synergy in a single click
+ * Copyright (C) 2005-2009 Otávio Cordeiro. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
 
 #import "SynergyHelper.h"
 
